73043b663a6618e54ab4db9a42a2145f77731269
Evan Smith <professionalemail@zoho.com>
2020-04-16 00:16:13 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
 
+require 'pry'
+
+class CashRegister
+attr_accessor :total, :discount
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity=nil)
+    if not quantity == nil
+      value = price*quantity 
+    else
+      value = price
+    end
+    
+    @total += value
+  end
+end

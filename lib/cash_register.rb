50705db27ed31782b50631ea018c25c44d14e141
Evan Smith <professionalemail@zoho.com>
2020-04-16 02:50:42 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,20 +1,26 @@
-
 require 'pry'
 
 class CashRegister
-attr_accessor :total, :discount
+attr_accessor :total, :discount, :items
+
   def initialize(discount = nil)
     @total = 0
+    @items = []
     @discount = discount
   end
   
   def add_item(title, price, quantity=nil)
     if not quantity == nil
-      value = price*quantity 
+      value = price*quantity
+      quantity.times do
+        @items << title
+      end
     else
       value = price
+      @items << title
     end
     @total += value
+    
   end
   
   def apply_discount
@@ -22,10 +28,9 @@ attr_accessor :total, :discount
     discount = @discount*10
     @total = @total - discount
     "After the discount, the total comes to $#{total}."
-  else
+   else
     "There is no discount to apply."
+   end
   end
-  end
-  
   
 end

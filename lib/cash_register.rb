19b4f76f8d8eece2ee63b40f6fe1c733647a94c7
shamellakin <christlife4u@gmail.com>
2020-04-17 15:09:04 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class CashRegister 
     attr_accessor :total, :discount
   
@@ -5,11 +7,14 @@ class CashRegister
     def initialize(discount = 0)
       @total = 0
       @discount = discount
-      
+      @@items_list = []
     end
     
-    def add_item(title, price)
-     self.total += (title * price)
+    def add_item(title, price, quantity = 1)
+        self.total += (price * quantity)
+        binding.pry
+
+        # items_list << (price * quantity)
     end
       
     

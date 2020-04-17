e6a56201b915b3c6a7678bf99b525f5ca04e9d9f
shamellakin <christlife4u@gmail.com>
2020-04-17 11:51:12 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
+class CashRegister
+    
+    attr_accessor :total
+    attr_reader :discount
+    
+    def initialize(total= 0, discount= 20)
+        @total = total
+        @discount = discount
+    end 
 
+    
+end 

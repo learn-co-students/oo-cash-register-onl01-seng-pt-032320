a2fe37fc8d7dd01a2a3b311497d53430cd60a7c0
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-19 20:32:35 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,29 +10,34 @@ class CashRegister
     @items = []
   end
 
-#  def total
-    #@items.each{ |name, price, quantity| self.total += price * quantity}
-    #@total
-  #end
-  
   def add_item(name, price, quantity = 1) #each ITEM = [name,price,quantity]
     item = [name, price, quantity]
     self.total += price * quantity
     @items << item
   end
     
-    def apply_discount
-      if discount !=0
-        discount = @total * (@discount.to_f/100)
-        @total = @total - discount
-        "After the discount, the total comes to $#{@total.to_i}."
-      else
-        "There is no discount to apply."
-      end
+  def apply_discount
+    if discount !=0
+      discount = @total * (@discount.to_f/100)
+      @total = @total - discount
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
     end
+  end
     
-    def items
-     binding.pry
-     @items.collect{ |item| (item[0].split * item[2])}.flatten
-    end
+  def items
+   @items.collect{ |item| (item[0].split * item[2])}.flatten
+  end
+  
+  def total
+  #binding.pry
+  @items.collect{ |name, price, quantity| @total += price * quantity}
+  @total
+  end
+    
+  def void_last_transaction
+    #binding.pry
+    @items.pop
+  end
 end

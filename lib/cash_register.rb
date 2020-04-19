893a1d6f15856c2e548a687a4ed7ea1c06713329
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-19 03:20:18 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+require 'pry'
 
+class CashRegister
+  
+  attr_writer :total
+  attr_reader :discount
+  
+  
+  def initializate(*discount)
+    @total = 0
+    @discount = discount
+  end
+
+  def total
+    @total * @discount
+  end
+  
+  def add_item(item, price, *quantity)
+    self.total += price * quantity
+  end
+    
+  
+  
+end
